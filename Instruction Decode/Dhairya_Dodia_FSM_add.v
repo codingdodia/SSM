f38661a9a79544f8887b07be032abc6d29a263be
codingdodia <dhairyadodia2024@gmail.com>
2024-12-02 18:42:52 -0600

    added 4 registers in the register module

--- Instruction Decode/Dhairya_Dodia_FSM_add.v
+++ Instruction Decode/Dhairya_Dodia_FSM_add.v
@@ -1,7 +1,11 @@
 module Add_FSM(
     input clock,
     input reset,
+    input [6:0] param1;
+    input [6:0] param2;
     output done
-)
+);
+
+
 
 
--- Registers/Dhairya_Dodia_Register_Module.v
+++ Registers/Dhairya_Dodia_Register_Module.v
@@ -1,25 +1,42 @@
 module Register(
     input register_clock,
     input register_reset,
+    input register_addr;
     input [15:0] bus_register_input,
     input bus_register_input_en,
     output [15:0] bus_register_output,
     output bus_register_out_en
 );
 
-
+reg [15:0] register_data [3:0];
 reg [15:0] register_data;
 
 always@(posedge register_clock) begin
 
     if(register_reset) begin
-        register_data <= 16'b0;
+         register_data[0] <= 16'b0;
+        register_data[1] <= 16'b0;
+        register_data[2] <= 16'b0;
+        register_data[3] <= 16'b0;
     end
 
     else if(bus_register_input_en) begin
-        register_data <= bus_register_input
+         2'b00: register_data[0] <= bus_register_input;
+        2'b01: register_data[1] <= bus_register_input;
+        2'b10: register_data[2] <= bus_register_input;
+        2'b11: register_data[3] <= bus_register_input;
     end
 
+end
+
+always @(*) begin
+    case (register_addr)
+        2'b00: selected_register_data = register_data[0];
+        2'b01: selected_register_data = register_data[1];
+        2'b10: selected_register_data = register_data[2];
+        2'b11: selected_register_data = register_data[3];
+        default: selected_register_data = 16'b0;
+    endcase
 end
 
 tri_state_buffer register_tri_state(
