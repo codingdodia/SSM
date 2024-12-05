beb2ecb8298b53668dc68bafcbc0eaef18bbdd7c
codingdodia <dhairyadodia2024@gmail.com>
2024-12-05 04:10:52 -0600

    IO_Zero updated

--- Registers/Dhairya_Dodia_IO_0.v
+++ Registers/Dhairya_Dodia_IO_0.v
@@ -2,35 +2,34 @@ module IO_Zero(
     input I0_clock,
     input I0_reset,
     input [15:0] bus_I0_input,
-    input bus_IO_input_en,
+    input bus_I0_input_en,
     output [15:0] bus_I0_output,
     input bus_I0_output_en,
-    output [15:0] external_IO_Zero_output,
-    output external_IO_Zero_output_en
+    output reg [15:0] external_I0_output
 );
 
 reg [15:0] I0_data;
 
-always@(posedge IO_clock) begin
+always@(posedge I0_clock) begin
     
-    if(IO_reset) begin
-        I0_data;
+    if(I0_reset) begin
+        I0_data = 16'b0;
     end
 
-    else if(bus_IO_input_en) begin
-        IO_data <= bus_IO_Zero_input;
+    else if(bus_I0_input_en) begin
+        I0_data = bus_I0_input;
     end
 
 end
 
-tri_state_buffer IO_Zero_bus_tri_state(
-    .data_in(IO_data),
+tri_state_buffer I0_bus_tri_state(
+    .data_in(I0_data),
     .enable(bus_I0_output_en),
     .data_out(bus_I0_output)
 );
 
 always@(*) begin
-    external_IO_Zero_output = IO_data;
+    external_I0_output = I0_data;
 end
 
 endmodule
