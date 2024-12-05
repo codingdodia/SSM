cb15a4bb1a51135b5b5dfc084f7287ede7181ec9
codingdodia <dhairyadodia2024@gmail.com>
2024-12-05 04:10:17 -0600

    MAR updated

--- Registers/Dhairya_Dodia_MAR.v
+++ Registers/Dhairya_Dodia_MAR.v
@@ -1,21 +1,25 @@
 module MAR(
     input MAR_clock,
     input MAR_reset,
-    input MAR_in_en,
     input address_in_en,
     input [15:0] MAR_address_in,
-    output reg [15:0] MAR_address_out
+    output reg [15:0] MAR_address_out,
+    output reg [15:0] MAR_data
 );
+
 
 always @(posedge MAR_clock) begin
     if (MAR_reset) begin
-        address_out <= 16'b0;
+        MAR_address_out = 16'b0;
     end
 
-    else if(MAR_in_en) begin
-        address_out <= address_in;
+    else if(address_in_en) begin
+        MAR_data = MAR_address_in;
     end
 end
 
+always@(*) begin
+    MAR_address_out = MAR_data;
+end
 
 endmodule
